7c744109049d358a13c917b409cf4bf9b989c5b5
Nick Bumbarger <nbumbarger@gmail.com>
2017-01-25 15:12:12 -0500

    Remove Development Seed references

--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -10,10 +10,7 @@ var Home = React.createClass({
 
   render: function () {
     return (
-      <div className='header'>
-        <h1><img src='/assets/graphics/layout/ds-logo-pos.svg' alt='Development Seed logotype' width='188' height='32' /></h1>
-        <p>In the beginning the Universe was created.</p>
-        <p>This has made a lot of people very angry and been widely regarded as a bad move.</p>
+      <div>
       </div>
     );
   }
