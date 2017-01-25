c56ac1436810a3d1752e1e24a4e7942a822abe8a
Nick Bumbarger <nbumbarger@gmail.com>
2017-01-25 18:07:54 -0500

    Attach header and footer

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -7,7 +7,9 @@ const PageFooter = React.createClass({
   render: function () {
     return (
       <div className='page__footer'>
-        <h1>Copyright IFPRI 2017</h1>
+        <div className='contact'>
+          <p className='contact__item'><span>Copyright IFPRI 2017. </span> <a title='contact us' href='mailto:' className='link--primary'>Contact Us</a></p>
+        </div>
       </div>
     )
   }
