b8440b2d7d6f4a5ce1911ed463f0c78a1a9ab949
Nick Bumbarger <nbumbarger@gmail.com>
2017-01-26 18:04:22 -0500

    Update projects processing build script to include metadata

--- .build_scripts/projects.js
+++ .build_scripts/projects.js
@@ -1,10 +1,21 @@
 var fs = require('fs')
 var glob = require('glob')
+var path = require('path')
+var jsyaml = require('js-yaml')
 
 glob('app/assets/data/projects/*.md', function (err, files) {
   if (err) console.warn(err)
-  fs.writeFileSync('app/assets/data/projects.json',
-    JSON.stringify(files.map(function (file) {
-      return file.replace('app/', '')
-    })))
+  var inventory = {}
+  files.map(function (file) {
+    var metadata = fs.readFileSync(file).toString()
+    metadata = jsyaml.load(metadata.split('---')[1])
+    metadata = {
+      title: metadata.title,
+      tags: metadata.tags,
+      scenarios: metadata.scenarios,
+      url: file.replace('app/', '')
+    }
+    inventory[path.basename(file, '.md')] = metadata
+  })
+  fs.writeFile('./app/assets/data/projects.json', JSON.stringify(inventory))
 })
