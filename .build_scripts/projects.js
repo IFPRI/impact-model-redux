7587485dedd8abbd5f357f66cced4c4e3a23338f
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-01 13:59:12 -0500

    Add article preview to metadata generation

--- .build_scripts/projects.js
+++ .build_scripts/projects.js
@@ -3,11 +3,19 @@ var glob = require('glob')
 var path = require('path')
 var fm = require('front-matter')
 
+// Extract n charecters of preview text, rounded to the closest full word
+function cutAt (text, charecters) {
+  console.log(text)
+  var lastCharecter = text.lastIndexOf(' ', charecters)
+  return text.substring(0, lastCharecter) + '...'
+}
+
 glob('app/assets/data/projects/*.md', function (err, files) {
   if (err) console.warn(err)
   var inventory = {}
   files.map(function (file) {
-    var metadata = fm(fs.readFileSync(file).toString()).attributes
+    var text = fm(fs.readFileSync(file).toString())
+    var metadata = text.attributes
     metadata = {
       title: metadata.title,
       author: metadata.author,
@@ -18,7 +26,8 @@ glob('app/assets/data/projects/*.md', function (err, files) {
       tags: metadata.tags,
       resources: metadata.resources,
       locations: metadata.locations,
-      scenarios: metadata.scenarios
+      scenarios: metadata.scenarios,
+      preview: cutAt(text.body, 300).replace(/# /g, '').replace(/\n\n/g, ' ').replace(/\n/g, ' ').replace('....', '...').replace(/. #.../g, '...')
     }
     inventory[path.basename(file, '.md')] = metadata
   })
