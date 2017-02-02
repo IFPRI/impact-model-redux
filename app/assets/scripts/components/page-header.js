a19b1e174d1dc4e4fc066461df132f94855811e8
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-02 12:35:40 -0500

    Add header nav

--- app/assets/scripts/components/page-header.js
+++ app/assets/scripts/components/page-header.js
@@ -1,12 +1,19 @@
 'use strict'
 import React from 'react'
+import { Link } from 'react-router'
 
 const PageHeader = React.createClass({
   render: function () {
     return (
-      <div className='page__header'>
-        <h1>IFPRI IMPACT Model</h1>
-      </div>
+      <nav className='page__header'>
+        <h1>IFPRI IMPACT</h1>
+        <ul className='page__prime-nav'>
+          <li><Link to={'/briefs'}>Briefs</Link></li>
+          <li><Link to={'/scenarios'}>Scenarios</Link></li>
+          <li><Link to={'/about'}>About</Link></li>
+          <li><Link to={'/contact'}>Contact</Link></li>
+        </ul>
+      </nav>
     )
   }
 })
