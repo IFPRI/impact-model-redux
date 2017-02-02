d6954eee112701637d024fc90473092c265ecb23
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-02 18:53:30 -0500

    Style cards in article list

--- app/assets/scripts/components/list-article-card.js
+++ app/assets/scripts/components/list-article-card.js
@@ -15,23 +15,21 @@ const ListArticleCard = React.createClass({
     const date = moment(article.date, 'MM/DD/YYYY').format('MMMM D, YYYY')
     const locations = article.locations
     return (
-      <div className='article-card--list'>
-        <header className='article-card__header--list'>
-          <div className="header--list__title">
+      <div className='article-list-card'>
+        <header className='article-list-card__header'>
+          <div className='article-list-card__title'>
             <h4><Link to={`/articles/${article.id}`}>{article.title}</Link></h4>
-          </div>
-          <div>
             <span>{date}</span>
           </div>
-          <dl>
-            <dt>Author: <Link to={`${article.author}`}>{article.author}</Link></dt>
-            <dt>{
-              `Location${locations.length > 1 ? 's' : ''}:
-              ${locations.length > 1 ? commaSeparate(locations) : locations}`
-            }</dt>
-          </dl>
+          <div className='article-list-card__meta'>
+            <dl>
+              <dt><b>Author:</b> <Link to={`${article.author}`}>{article.author}</Link></dt>
+              <dt><b>Location{locations.length > 1 ? 's' : ''}:</b> {locations.length > 1 ? commaSeparate(locations) : locations}
+              </dt>
+            </dl>
+          </div>
         </header>
-        <div className='article-card__body--list'>
+        <div className='article-list-card__body'>
           <p>{`${cutAtWord(article.preview, 190)}...`}</p>
         </div>
     </div>
