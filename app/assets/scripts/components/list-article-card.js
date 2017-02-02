208eba8003b44b492c125dde8a3d447d0e0312fd
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-02 18:06:23 -0500

    Format article cards with necessary data

--- app/assets/scripts/components/list-article-card.js
+++ app/assets/scripts/components/list-article-card.js
@@ -3,7 +3,7 @@ import React from 'react'
 import moment from 'moment'
 import { Link } from 'react-router'
 
-import { cutAtWord } from '../utils/format'
+import { commaSeparate, cutAtWord } from '../utils/format'
 
 const ListArticleCard = React.createClass({
   propTypes: {
@@ -13,16 +13,23 @@ const ListArticleCard = React.createClass({
   render: function () {
     const article = this.props.article
     const date = moment(article.date, 'MM/DD/YYYY').format('MMMM D, YYYY')
+    const locations = article.locations
     return (
       <div className='article-card--list'>
         <header className='article-card__header--list'>
-          <h4>
-            <Link to={`/articles/${article.id}`}>{article.title}</Link>
-          </h4>
-          <ul>
-            <li>{date}</li>
-            <li><Link to={`${article.author}`}>{article.author}</Link></li>
-          </ul>
+          <div className="header--list__title">
+            <h4><Link to={`/articles/${article.id}`}>{article.title}</Link></h4>
+          </div>
+          <div>
+            <span>{date}</span>
+          </div>
+          <dl>
+            <dt>Author: <Link to={`${article.author}`}>{article.author}</Link></dt>
+            <dt>{
+              `Location${locations.length > 1 ? 's' : ''}:
+              ${locations.length > 1 ? commaSeparate(locations) : locations}`
+            }</dt>
+          </dl>
         </header>
         <div className='article-card__body--list'>
           <p>{`${cutAtWord(article.preview, 190)}...`}</p>
