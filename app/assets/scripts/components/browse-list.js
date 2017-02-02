ec215f189bb57e88725d9d9684be49591f2f8171
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-02 15:38:42 -0500

    Scaffold article browse pages

---
+++ app/assets/scripts/components/browse-list.js
@@ -0,0 +1,19 @@
+'use strict'
+import React from 'react'
+
+const BrowseList = React.createClass({
+  propTypes: {
+  },
+
+  render: function () {
+    return (
+      <div className='browse__article-list'>
+        <header className='article-list__header'>
+          <h2>Results</h2>
+        </header>
+      </div>
+    )
+  }
+})
+
+export default BrowseList
--- app/assets/scripts/views/scenario-browse.js
+++ app/assets/scripts/views/scenario-browse.js
@@ -1,6 +1,9 @@
 'use strict'
 import React from 'react'
 
+import BrowseFilters from '../components/browse-filters.js'
+import BrowseList from '../components/browse-list.js'
+
 const ScenarioBrowse = React.createClass({
   propTypes: {
   },
@@ -8,9 +11,18 @@ const ScenarioBrowse = React.createClass({
   render: function () {
     return (
       <div className='page__browse'>
-        <div className='browse__header'>
-          <h2>Scenarios</h2>
-        </div>`
+        <header className='browse__header'>
+          <div className='browse__header-text'>
+            <h2>Scenarios</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse ut augue aliquet ligula aliquam. Lorem ipsum dolor
+              sit amet, consectetur adipiscing elit. Suspendisse ut augue
+              aliquet ligula aliquam.
+            </p>
+          </div>
+        </header>
+        <BrowseFilters />
+        <BrowseList />
       </div>
     )
   }
