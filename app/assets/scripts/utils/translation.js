bfdc7d783f6c0c42f785f86f315f71a63e52d414
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-03 17:19:29 -0500

    Add aggregate data inversion functions

--- app/assets/scripts/utils/translation.js
+++ app/assets/scripts/utils/translation.js
@@ -6,3 +6,42 @@ import { translationData } from '../../data/translation'
 export const translate = (str) => translationData[str]
 
 export const untranslate = (str) => _.invert(translationData)[str]
+
+export const invertCommodities = (commodities) => {
+  const inverted = {}
+  _.forEach(commodities, (category, item) => {
+    if (!inverted[category]) inverted[category] = []
+    inverted[category].push(item)
+  })
+  return inverted
+}
+
+export const countryIdsToContinents = (countries) => {
+  const continents = _.map(countries, (attributes, id) => {
+    const continent = attributes.continent
+    if (!continents[continent]) {
+      continents[continent] = []
+    }
+    return {
+      id: id,
+      region: attributes.region,
+      subcontinent: attributes.subcontinent
+    }
+  })
+  return continents
+}
+
+export const countryIdsToSubontinents = (countries) => {
+  const subcontinents = _.map(countries, (attributes, id) => {
+    const subcontinent = attributes.subcontinent
+    if (!subcontinents[subcontinent]) {
+      subcontinents[subcontinent] = []
+    }
+    return {
+      id: id,
+      region: attributes.region,
+      continent: attributes.continent
+    }
+  })
+  return subcontinents
+}
