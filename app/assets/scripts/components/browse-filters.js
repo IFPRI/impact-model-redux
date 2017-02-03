71f2ae06a85780dfe49b93a07faf96e1e6029605
Nick Bumbarger <nbumbarger@gmail.com>
2017-02-03 18:05:54 -0500

    Start building filter sidebar component

--- app/assets/scripts/components/browse-filters.js
+++ app/assets/scripts/components/browse-filters.js
@@ -1,6 +1,8 @@
 'use strict'
 import React from 'react'
 
+import { commodities } from '../../data/aggregate-commodity'
+
 const BrowseFilters = React.createClass({
   propTypes: {
   },
@@ -11,6 +13,22 @@ const BrowseFilters = React.createClass({
         <header className='filters__header'>
           <h2>Filter</h2>
         </header>
+        <form className='filters__form'>
+          <h3 className='filters__group-label'>Type</h3>
+          <div className='filters__check-group'>
+            <input type='checkbox' name='custom' value='custom' />
+            Custom
+          </div>
+          <div className='filters__check-group'>
+            <input type='checkbox' name='country-summary' value='country-summary' />
+            Country Summary
+          </div>
+          <div className='filters__check-group'>
+            <input type='checkbox' name='commodity-summary' value='commodity-summary' />
+            Commodity Summary
+          </div>
+          <h3 className='filters__group-label'>Commodities</h3>
+        </form>
       </div>
     )
   }
