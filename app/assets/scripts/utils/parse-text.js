8a93939023af4edcec279bc727f89475fd0968b0
Nick Bumbarger <nbumbarger@gmail.com>
2017-03-06 17:19:43 -0500

    Begin refactoring chart renderer to save figures to state

--- app/assets/scripts/utils/parse-text.js
+++ app/assets/scripts/utils/parse-text.js
@@ -4,14 +4,16 @@ import yaml from 'js-yaml'
 import makeChart from './make-chart'
 import makeMap from './make-map'
 
+import { updateFigures } from '../actions'
+
 export const setupRenderer = () => {
   const renderer = new marked.Renderer()
   renderer.code = (code, lang, escaped) => {
     const data = yaml.load(code)
     if (lang === 'chart') {
-      return makeChart(data)
+      return `<figure class="chart-${data.title}"></figure>`
     } else if (lang === 'map') {
-      return makeMap(data)
+      return `<figure class="map-${data.title}"></figure>`
     }
   }
   return renderer
