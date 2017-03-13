acfc1168c7003918aa647d49e2e5679f876a9a16
Drew Bollinger <drew@developmentseed.org>
2017-03-13 11:16:44 -0400

    Remove jquery, start removing unneeded code

--- app/assets/scripts/components/map.js
+++ app/assets/scripts/components/map.js
@@ -2,30 +2,11 @@
 import React from 'react'
 import d3 from 'd3'
 import _ from 'lodash'
-import $ from 'jquery'
 import sqlToES from 'sqltoes'
 import topojson from 'topojson'
 import d3geo from 'd3-geo-projection'
 
-import MapChoro from '../utils/mapchoro'
-import { translate, untranslate } from '../utils/translation'
-
 export class Map extends React.Component {
-  // initialize (options) {
-  //   this.loaded = 0
-  //   this.scenario = options.scenario
-  //   this.yearRange = options.yearRange
-  //
-  //   this.uiCollections = ['mapChoro', 'dropdown', 'dropdown2', 'dropdown3']
-  //   _.each(this.uiCollections, function (x) {
-  //     this.listenTo(this.collection[x], 'sync', this.load)
-  //   }.bind(this))
-  //   this.listenTo(this.collection.mapChoro, 'reset', this.load)
-  //   this.full = this.uiCollections.length
-  //
-  //   var that = this
-  //
-  //   this.spinner = new Spinner(IfpriImpact.spinnerOptions)
   //   this.mapTip = d3.tip()
   //     .attr('class', 'd3-tip')
   //     .offset(function (d) {
@@ -61,15 +42,6 @@ export class Map extends React.Component {
   //       var front = (that.findMapInfo(d.id) > 0) ? '+' : ''
   //       return '<strong>' + translate(d.id) + '</strong><br>Change: ' + front + (that.getDiff(that.findMapInfo(d.id)) * 100).toFixed(1) + '%'
   //     })
-  // }
-  //
-  // load () {
-  //   this.loaded += 1
-  //   if (this.loaded === this.full) {
-  //     this.render()
-  //   } else if (this.loaded > this.full) {
-  //     this.drawMap()
-  //   }
   // }
   //
   // render () {
@@ -626,60 +598,8 @@ export class Map extends React.Component {
     return (
       <figure className='map'>
         <figcaption>The map shows change in key output parameters from across geographies. Use dropdown menus to select desired commodity (or group) and parameters to display. Toggle buttons at top right allow different geographic aggregations. Hover over countries or regions to observe the actual results.</figcaption>
-        <div className='row'>
-          <nav className='navbar navbar-default navbar-view'>
-            <div className='container'>
-              <div className='navbar-header'>
-                <a className='navbar-brand no-point'>Map</a>
-              </div>
-              <ul className='nav navbar-nav'>
-                <li className='dropdown'>
-                  <a className='dropdown-toggle' data-toggle='dropdown' id='dropdownMenu1' type='button'>Commodities<span className='caret'></span></a>
-                  <ul className='dropdown-menu scrollable-menu' data-type='agg-commodity'>
-                    {/* <% _.each(groups, function(group){ %> */}
-                    <li className='dropdown-submenu menu-map'>
-                      {/* <a tabindex='-1'><%= translate(group.name) %></a> */}
-                      <a tabIndex='-1'></a>
-                      <ul className='dropdown-menu' data-type='commodity'>
-                        {/* <% _.each(group.array, function(el){ %> */}
-                        <li className='menu-map'><a tabIndex='-1'></a></li>
-                        {/* <li className='menu-map'><a tabindex='-1'><%= translate(el) %></a></li> */}
-                        {/* <% }) %> */}
-                      </ul>
-                    </li>
-                    {/* <%  }) %> */}
-                  </ul>
-                </li>
-                <li className='dropdown'>
-                  <a className='dropdown-toggle' data-toggle='dropdown' id='dropdownMenu3' type='button'>IMPACT Results <span className='caret'></span></a>
-                  <ul className='dropdown-menu scrollable-menu' data-type='description'>
-                    {/* <% _.each(description, function(x){ %> */}
-                    {/* <li className='description-menu-map menu-map'><a tabindex='-1'><%= translate(x) %></a></li> */}
-                    <li className='description-menu-map menu-map'><a tabIndex='-1'></a></li>
-                    {/* <%  }) %> */}
-                  </ul>
-                </li>
-              </ul>
-              <ul className='nav navbar-nav navbar-right'>
-                <li><a className='level-toggle' id='region'>Country Level</a></li>
-                <li><a className='level-toggle' id='agg_subcontinent'>Sub-Continent Level</a></li>
-                <li><a className='level-toggle' id='agg_continent'>Continent Level</a></li>
-              </ul>
-              </div>
-            </nav>
-          </div>
-        <div className='container'>
-          <div className='row'>
+        <div className='map-container'>
             <div id='world-map'></div>
-          </div>
-          <div className='help-text'>
-            <a className='close-help' href='#'>x</a>
-            <h2>
-              Map
-              </h2>The map shows change in key output parameters from across geographies. Use dropdown menus to select desired commodity (or group) and parameters to display. Toggle buttons at top right allow different geographic aggregations. Hover over countries or regions to observe the actual results.
-            {/* </h2>The map shows change in key output parameters from <%= yearRange[0] %> to <%= yearRange[1] %> across geographies. Use dropdown menus to select desired commodity (or group) and parameters to display. Toggle buttons at top right allow different geographic aggregations. Hover over countries or regions to observe the actual results. */}
-          </div>
-          <a className='show-help' href='#'>i</a>
         </div>
       </figure>
     )
