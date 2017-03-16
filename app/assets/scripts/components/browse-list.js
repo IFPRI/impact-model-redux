cb7b38ceb20d0311c02b19f3600532d5f4691f2f
Drew Bollinger <drew@developmentseed.org>
2017-03-16 17:56:02 -0400

    Update last page logic

--- app/assets/scripts/components/browse-list.js
+++ app/assets/scripts/components/browse-list.js
@@ -81,10 +81,9 @@ export class BrowseList extends React.Component {
     const articleCount = articles.length
     articles = articles.slice(articleBrowsePageLength * articlePage, articleBrowsePageLength * articlePage + articleBrowsePageLength)
 
-    const highArticle = Math.min(articleCount, articleBrowsePageLength * articlePage + articleBrowsePageLength + 1)
-
     // show first/last/two on each side
-    const lastPage = Math.floor(articleCount / articleBrowsePageLength)
+    const lastPage = Math.floor(articleCount / articleBrowsePageLength) - 1
+
     const pages = _.uniq([
       0,
       lastPage,
@@ -98,7 +97,7 @@ export class BrowseList extends React.Component {
 
     // add ellipses
     if (pages[1] !== 1) pages.splice(1, 0, '...')
-    if (pages[pages.length - 2] !== lastPage - 1) pages.splice(pages.length - 1, 0, '...')
+    if (pages[pages.length - 2] !== lastPage) pages.splice(pages.length - 1, 0, '...')
 
     const ClearFilters = articleFilters.length
     ? <a className='filter__selects__clear link__underline' href='' onClick={this.clearFilters}>Clear All Filters</a>
@@ -149,7 +148,7 @@ export class BrowseList extends React.Component {
             ? <li key={`ellipses-${i}`} className='browse__pagination-button'><a href=''>{page}</a></li>
             : <li key={page} className={c('browse__pagination-button', { active: articlePage === (page - 1) })}><a onClick={(e) => this.goToPage(page - 1, e)} href=''>{page}</a></li>
           })}
-          <li className={c('browse__pagination-button', 'browse__pagination-button--forward', 'collecticon-chevron-right', {'pagination-button--disabled': highArticle === articleCount})}>
+          <li className={c('browse__pagination-button', 'browse__pagination-button--forward', 'collecticon-chevron-right', {'pagination-button--disabled': articlePage === lastPage})}>
             <a className='links-next-prev' onClick={(e) => this.goToPage(articlePage + 1, e)} href=''>Next</a>
           </li>
         </ul>
