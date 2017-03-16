7676f542a12c5a3fb616e949a31c4ea540f150e9
Drew Bollinger <drew@developmentseed.org>
2017-03-16 17:50:36 -0400

    Add active class, prevent default

--- app/assets/scripts/components/browse-list.js
+++ app/assets/scripts/components/browse-list.js
@@ -1,6 +1,6 @@
 'use strict'
 import React from 'react'
-import classnames from 'classnames'
+import c from 'classnames'
 import _ from 'lodash'
 
 // Actions
@@ -34,7 +34,8 @@ export class BrowseList extends React.Component {
     }
   }
 
-  goToPage (page) {
+  goToPage (page, e) {
+    if (e) e.preventDefault()
     this.props.dispatch(updateArticlePage(page))
   }
 
@@ -140,18 +141,16 @@ export class BrowseList extends React.Component {
           )
         })}
         <ul className='browse__pagination'>
-          <li
-            className={classnames('browse__pagination-button', 'browse__pagination-button--back', 'collecticon-chevron-left', {'pagination-button--disabled': articlePage === 0})}
-            onClick={() => this.goToPage(articlePage - 1)}><a className='links-next-prev' href=''>Previous</a>
+          <li className={c('browse__pagination-button', 'browse__pagination-button--back', 'collecticon-chevron-left', {'pagination-button--disabled': articlePage === 0})}>
+            <a className='links-next-prev' onClick={(e) => this.goToPage(articlePage - 1, e)} href=''>Previous</a>
           </li>
           {pages.map((page, i) => {
             return isNaN(page)
             ? <li key={`ellipses-${i}`} className='browse__pagination-button'><a href=''>{page}</a></li>
-            : <li key={page} className='browse__pagination-button' onClick={() => this.goToPage(page)}><a href=''>{page}</a></li>
+            : <li key={page} className={c('browse__pagination-button', { active: articlePage === (page - 1) })}><a onClick={(e) => this.goToPage(page - 1, e)} href=''>{page}</a></li>
           })}
-          <li
-            className={classnames('browse__pagination-button', 'browse__pagination-button--forward', 'collecticon-chevron-right', {'pagination-button--disabled': highArticle === articleCount})}
-            onClick={() => this.goToPage(articlePage + 1)}><a className='links-next-prev' href=''>Next</a>
+          <li className={c('browse__pagination-button', 'browse__pagination-button--forward', 'collecticon-chevron-right', {'pagination-button--disabled': highArticle === articleCount})}>
+            <a className='links-next-prev' onClick={(e) => this.goToPage(articlePage + 1, e)} href=''>Next</a>
           </li>
         </ul>
       </section>
