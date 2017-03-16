7ab5a53b92506dd4d69c1c8a5d47fb1d047f6685
Drew Bollinger <drew@developmentseed.org>
2017-03-16 11:17:44 -0400

    Translate brief types

--- app/assets/scripts/views/brief.js
+++ app/assets/scripts/views/brief.js
@@ -70,7 +70,7 @@ export class Brief extends React.Component {
           <div className='row row--shortened'>
             <div className='home__header-split--left split__internal--left'>
               <div className='home__header-split--left__content'>
-                <span className='header--type'>{articleMetadata.briefType.replace(/-/g, ' ')}</span>
+                <span className='header--type'>{translate(articleMetadata.briefType)}</span>
                 <h2 className='header--xxlarge with-metadata'>{articleMetadata.title}</h2>
                 <dl className='article-byline header__metadata'>
                   <dt className='visually-hidden'>Date</dt>
