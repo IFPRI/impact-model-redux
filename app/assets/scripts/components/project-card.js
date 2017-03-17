57beff61bdd256af06cca4b28a66958bdef4d00f
Drew Bollinger <drew@developmentseed.org>
2017-03-17 13:40:24 -0400

    Render featured projects dynamically

---
+++ app/assets/scripts/components/project-card.js
@@ -0,0 +1,34 @@
+'use strict'
+import React from 'react'
+
+class ProjectCard extends React.Component {
+
+  goToScenarios (project, e) {
+    e.preventDefault()
+    this.props.updateArticleFilters([project])
+    this.props.router.push(`/scenarios`)
+  }
+
+  render () {
+    const { project } = this.props
+
+    return (
+      <li className='featured-project__item featured-project__item--settings'>
+        <div className='featured-project__item--body'>
+          <h4 className='header--large'>{project}</h4>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a molestie sapien. Sed ac nunc vel risus luctus suscipit ut ut dolor. Etiam sit amet elit volutpat, tempus nisl non, sem. Etiam sit amet elit volutpat, tempus nisl non…</p>
+          <a href="" className='link-block' onClick={this.goToScenarios.bind(this, project)}>View All Related Scenarios</a>
+        </div>
+      </li>
+    )
+  }
+}
+
+// Set default props
+ProjectCard.propTypes = {
+  project: React.PropTypes.string,
+  router: React.PropTypes.object,
+  updateArticleFilters: React.PropTypes.func
+}
+
+export default ProjectCard
--- app/assets/scripts/views/home.js
+++ app/assets/scripts/views/home.js
@@ -3,9 +3,26 @@ import React from 'react'
 import { Link } from 'react-router'
 import { connect } from 'react-redux'
 
+// Actions
+import { updateArticleFilters } from '../actions'
+
+// Components
 import RelatedArticles from '../components/related-articles'
+import FeaturedProjects from '../components/featured-projects'
+
+// Data
+import filterCategories from '../../data/filter-categories'
 
 export class Home extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+    this.updateArticleFilters = this.updateArticleFilters.bind(this)
+  }
+
+  updateArticleFilters (filters) {
+    this.props.dispatch(updateArticleFilters(filters))
+  }
+
   render () {
     return (
       <section className='page__home'>
@@ -23,31 +40,12 @@ export class Home extends React.Component {
             </div>
           </div>
         </header>
-        <section className='section__internal'>
-          <div className='row row--shortened'>
-            <header className='header-internal'>
-              <h3 className='header--xlarge with-description'>Featured Projects</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris et dui gravida, posuere diam id, congue augue. Pellentesque nec purus ex.</p>
-            </header>
-            <ul>
-              <li className='featured-project__item featured-project__item--settings'>
-                <div className='featured-project__item--body'>
-                  <h4 className='header--large'>Drought Tolerant Beans and Climate Change</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a molestie sapien. Sed ac nunc vel risus luctus suscipit ut ut dolor. Etiam sit amet elit volutpat, tempus nisl non, sem. Etiam sit amet elit volutpat, tempus nisl non…</p>
-                  <Link className='link-block' to={'/'}>View All Related Scenarios</Link>
-                </div>
-              </li>
-              <li className='featured-project__item featured-project__item--energy'>
-                <div className='featured-project__item--body'>
-                  <h4 className='header--large'>Drought Tolerant Beans and Climate Change</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a molestie sapien. Sed ac nunc vel risus luctus suscipit ut ut dolor. Etiam sit amet elit volutpat, tempus nisl non, sem. Etiam sit amet elit volutpat, tempus nisl non…</p>
-                  <Link className='link-block' to={'/'}>View All Related Scenarios</Link>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </section>
-        <RelatedArticles title="Recently Added Briefs" articles={this.props.briefs.sort((a, b) => b.date - a.date).slice(0, 3)}>
+        <FeaturedProjects
+          projects={filterCategories.projects}
+          udpateArticleFilter={this.updateArticleFilters}
+          router={this.props.router}
+          />
+        <RelatedArticles title="Recently Added Briefs" cardType='related' articles={this.props.briefs.sort((a, b) => b.date - a.date).slice(0, 3)}>
           <Link className='button button--main button--more-information' to={'/briefs'}>View All Briefs</Link>
         </RelatedArticles>
       </section>
@@ -56,13 +54,16 @@ export class Home extends React.Component {
 }
 
 Home.propTypes = {
-  briefs: React.PropTypes.array
+  briefs: React.PropTypes.array,
+  dispatch: React.PropTypes.func,
+  router: React.PropTypes.object
 }
 
 const mapStateToProps = (state) => {
   return {
     briefs: state.article.briefs,
-    scenarios: state.article.scenarios
+    scenarios: state.article.scenarios,
+    projects: state.articles
   }
 }
 export default connect(mapStateToProps)(Home)
