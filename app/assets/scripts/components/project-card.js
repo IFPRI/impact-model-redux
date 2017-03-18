09c929c9e728828d4cececc0827d0dd2a5c3fd04
Drew Bollinger <drew@developmentseed.org>
2017-03-18 19:07:30 -0400

    Translate project name on homepage

--- app/assets/scripts/components/project-card.js
+++ app/assets/scripts/components/project-card.js
@@ -1,6 +1,8 @@
 'use strict'
 import React from 'react'
 
+import { translate } from '../utils/translation'
+
 class ProjectCard extends React.Component {
 
   goToScenarios (project, e) {
@@ -15,7 +17,7 @@ class ProjectCard extends React.Component {
     return (
       <li className='featured-project__item featured-project__item--settings'>
         <div className='featured-project__item--body'>
-          <h4 className='header--large'>{project}</h4>
+          <h4 className='header--large'>{translate(project)}</h4>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce a molestie sapien. Sed ac nunc vel risus luctus suscipit ut ut dolor. Etiam sit amet elit volutpat, tempus nisl non, sem. Etiam sit amet elit volutpat, tempus nisl non…</p>
           <a href="" className='link-block' onClick={this.goToScenarios.bind(this, project)}>View All Related Scenarios</a>
         </div>
