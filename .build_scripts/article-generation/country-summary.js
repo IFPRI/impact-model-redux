02a2e2a62fd09b9215e04d84e5e63e9c0146b175
Drew Bollinger <drew@developmentseed.org>
2017-03-20 01:22:01 -0400

    Update country generation to query data

--- .build_scripts/article-generation/country-summary.js
+++ .build_scripts/article-generation/country-summary.js
@@ -1,14 +1,20 @@
 'use strict'
 var fs = require('fs')
 var _ = require('lodash')
+var queue = require('queue-async')
+var sqltoes = require('sqltoes')
+var fetch = require('isomorphic-fetch')
 
 var translation = require('../../app/assets/scripts/utils/translation')
 var regions = require('../../app/assets/data/aggregate-region.json')
 var commodities = require('../../app/assets/data/aggregate-commodity.json')
 
 var baselineScenarios = ['SSP2_GFDL', 'SSP2_HGEM', 'SSP2_MIROC', 'SSP2_IPSL', 'SSP2_NOCC']
+var dbUrl = 'https://82e66fb22a73abdaa81040f2abdc298f-us-east-1.foundcluster.com:9243/ifpri/ssp2_gfdl/_search?search_type=count'
+var q = queue(2)
 
-function generateArticle (region, subcontinent, continent) {
+function generateArticle (region, subcontinent, continent, callback) {
+  console.log(`Creating country page for: ${translation.translate(region)}`)
   var date = new Date()
   date = `${date.getMonth() + 1}/${date.getDate()}/${1900 + date.getYear()}`
   var articleType = 'brief'
@@ -67,21 +73,62 @@ fixed:
   var foodSecurityFigures = ['populationatriskxagg', 'foodavailxagg', 'totalmalnourishedxagg']
     .map(param => `${foodSecurity(param)}\n\n`).join('')
 
-  var table = `|   |   | 2015 | 2030 | 2050 |
+  const neededDataWheres = [
+    [`agg_subcontinent = ${subcontinent}`, 'year = 2015', 'impactparameter = popxagg'],
+    [`agg_subcontinent = ${subcontinent}`, 'year = 2030', 'impactparameter = popxagg'],
+    [`agg_subcontinent = ${subcontinent}`, 'year = 2050', 'impactparameter = popxagg'],
+    [`agg_subcontinent = ${subcontinent}`, 'year = 2015', 'impactparameter = gdpxagg'],
+    [`agg_subcontinent = ${subcontinent}`, 'year = 2030', 'impactparameter = gdpxagg'],
+    [`agg_subcontinent = ${subcontinent}`, 'year = 2050', 'impactparameter = gdpxagg'],
+    [`region = ${region}`, 'year = 2015', 'impactparameter = popxagg'],
+    [`region = ${region}`, 'year = 2030', 'impactparameter = popxagg'],
+    [`region = ${region}`, 'year = 2050', 'impactparameter = popxagg'],
+    [`region = ${region}`, 'year = 2015', 'impactparameter = gdpxagg'],
+    [`region = ${region}`, 'year = 2030', 'impactparameter = gdpxagg'],
+    [`region = ${region}`, 'year = 2050', 'impactparameter = gdpxagg']
+  ]
+
+  var articleQueue = queue(12)
+  neededDataWheres.forEach((where, index) => {
+    const wherePath = where.map(a => {
+      let b, c
+      if (a.match(' = ')) {
+        [b, c] = a.split(' = ')
+        return `where_${b}_${c}`
+      } else {
+        [b, c] = a.split(' in ')
+        return `where_${b}_multiple`
+      }
+    })
+    articleQueue.defer(function (cb) {
+      const postData = JSON.stringify(sqltoes({select: [`sum(Val)`], where, groupBy: []}))
+      fetch(dbUrl, {
+        method: 'post',
+        body: postData
+      })
+      .then((resp) => resp.json())
+      .then((resp) => cb(null, _.get(resp.aggregations, wherePath).sum_Val.value.toFixed(2)))
+      .catch(err => callback(null, err))
+    })
+  })
+
+  articleQueue.awaitAll(function (err, results) {
+    if (err) console.log(err)
+    var table = `|   |   | 2015 | 2030 | 2050 |
 |---|---|---|---|---|
-| ${translation.translate(subcontinent)} | Population (million) |
-|  | GDP (billion $US) |
+| ${translation.translate(subcontinent)} | Population (million) | ${results[0]} | ${results[1]} | ${results[2]} |
+|  | GDP (billion $US) | ${results[3]} | ${results[4]} | ${results[5]} |
 |  | Per capita GDP ($US) |
-| ${translation.translate(region)} | Population (million) |
-|  | GDP (billion $US) |
+| ${translation.translate(region)} | Population (million) | ${results[6]} | ${results[7]} | ${results[8]} |
+|  | GDP (billion $US) | ${results[9]} | ${results[10]} | ${results[11]} |
 |  | Per capita GDP ($US) |`
 
-  var article = `Summary of IMPACT model outputs for ${name}\n\n${outputFigures}${foodSecurityFigures}${table}`
+    var article = `Summary of IMPACT model outputs for ${name}\n\n${outputFigures}${foodSecurityFigures}${table}`
 
-  var scenarioString = scenarios.map(s => ` - ${s}`).join('\n')
-  var tagString = tags.map(t => ` - ${t}`).join('\n')
+    var scenarioString = scenarios.map(s => ` - ${s}`).join('\n')
+    var tagString = tags.map(t => ` - ${t}`).join('\n')
 
-  var output = `---
+    var output = `---
 title: ${title}
 date: ${date}
 type: ${articleType}
@@ -98,9 +145,18 @@ ${tagString}
 ---
 ${article}`
 
-  fs.writeFileSync(`./app/assets/data/articles/${fileName}.md`, output)
+    fs.writeFileSync(`./app/assets/data/articles/${fileName}.md`, output)
+    callback(null, fileName)
+  })
 }
 
-_.forEach(regions, r => generateArticle(r.region, r.subcontinent, r.continent))
+_.forEach(regions, r => {
+  q.defer(cb => {
+    generateArticle(r.region, r.subcontinent, r.continent, cb)
+  })
+})
 
-console.log('Simulated articles saved to ./app/assets/data/articles')
+q.awaitAll(function (err, results) {
+  if (err) console.error('err: ' + err)
+  console.log('Simulated articles saved to ./app/assets/data/articles')
+})
