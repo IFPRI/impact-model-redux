52998dbdad7bd41f7c450f200f6fdd084c177704
Drew Bollinger <drew@developmentseed.org>
2017-03-21 00:00:52 -0400

    Add production endpoints

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -1,9 +1,6 @@
 'use strict'
-var logo = require('./logo')
-/*
- * App config for production.
- */
 module.exports = {
   environment: 'production',
-  consoleMessage: logo
+  dbUrl: 'https://82e66fb22a73abdaa81040f2abdc298f-us-east-1.foundcluster.com:9243/ifpri/ssp2_gfdl/_search?search_type=count',
+  baseUrl: 'http://devseed.com/ifpri-impact-redux'
 }
