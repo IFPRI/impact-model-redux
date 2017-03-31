059a8fac30706b69492fe71f33c084f1b736422f
Drew Bollinger <drew@developmentseed.org>
2017-03-31 11:55:18 -0400

    Add home reducer

---
+++ app/assets/scripts/reducers/home.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import { UPDATE_SELECTED_PROJECT } from './actions'
+
+export const initialState = {
+  selectedProject: null
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case UPDATE_SELECTED_PROJECT:
+      return { selectedProject: action.data }
+    default:
+      return state
+  }
+}
