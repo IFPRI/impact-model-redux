db752388e99b0fdc65a77990bdbb0b4c5b45e784
Drew Bollinger <drew@developmentseed.org>
2017-04-01 17:21:31 -0400

    Add mobile nav active class

--- app/assets/scripts/components/page-header.js
+++ app/assets/scripts/components/page-header.js
@@ -1,19 +1,56 @@
 'use strict'
 import React from 'react'
 import { Link } from 'react-router'
+import c from 'classnames'
 
 import { toTitleCase } from '../utils/format'
 
 export class PageHeader extends React.Component {
+  constructor (props, context) {
+    super(props, context)
+    this.state = {
+      mobileMenuOpen: false
+    }
+    this.mobileMenuClick = this.mobileMenuClick.bind(this)
+    this.documentListener = this.documentListener.bind(this)
+  }
+
+  documentListener (e) {
+    if (e.preventClose !== true && this.state.mobileMenuOpen) {
+      this.setState({mobileMenuOpen: false})
+    }
+  }
+
+  mobileMenuClick (e) {
+    e.preventDefault()
+    this.setState({mobileMenuOpen: !this.state.mobileMenuOpen})
+  }
+
+  onNavDataClick (e) {
+    // When clicking a nav block, add a property to the event indicating that
+    // the block shouldn't be toggled on body click.
+    e.preventClose = true
+  }
+
+  componentDidMount () {
+    document.addEventListener('click', this.documentListener)
+    this.navData.addEventListener('click', this.onNavDataClick)
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener('click', this.documentListener)
+    this.navData.removeEventListener('click', this.onNavDataClick)
+  }
+
   render () {
     const { page } = this.props
     const links = ['briefs', 'scenarios', 'about', 'contact']
     return (
       <header className='page__header'>
-        <div className='row'>
+        <div className='row' ref={v => (this.navData = v)}>
           <h1><Link to={'/'}>IFPRI IMPACT</Link></h1>
-          <Link className='mobile-menu' to={'#'}><span>Menu</span></Link>
-          <nav className='nav__main'>
+          <Link className='mobile-menu' onClick={this.mobileMenuClick} to={'/'}><span>Menu</span></Link>
+          <nav className={c('nav__main', { active: this.state.mobileMenuOpen })}>
             <ul className='page__prime-nav'>
               {links.map(link => <li key={link} className={page === link ? 'active' : ''}><Link to={`/${link}`}>{toTitleCase(link)}</Link></li>)}
             </ul>
