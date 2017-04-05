80a1d1b635af38fe726c9e7c7ee9c9b9d1982c57
Nick Bumbarger <nbumbarger@gmail.com>
2017-04-05 11:51:19 -0400

    Update DB url

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -1,6 +1,6 @@
 'use strict'
 module.exports = {
   environment: 'production',
-  dbUrl: 'https://82e66fb22a73abdaa81040f2abdc298f-us-east-1.foundcluster.com:9243/ifpri/ssp2_gfdl/_search?search_type=count',
+  dbUrl: 'https://82e66fb22a73abdaa81040f2abdc298f-us-east-1.foundcluster.com:9243/ifpri',
   baseUrl: 'http://devseed.com/ifpri-impact-redux'
 }
