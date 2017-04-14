9c830e19338276583702f973877eeb64a020cf3f
Nick Bumbarger <nbumbarger@gmail.com>
2017-04-14 16:27:54 -0400

    Make previewer use fluid layout

--- app/assets/scripts/components/previewer-error-modal.js
+++ app/assets/scripts/components/previewer-error-modal.js
@@ -8,20 +8,20 @@ export class ErrorModal extends React.Component {
     const error = this.props.error.split('^') ? this.props.error.split('^')[0] : this.props.error
     const height = document.querySelector('.previewer__output').scrollHeight
     return (
-        <div>
-          <CSSTransitionGroup
-            transitionName='fast-transition'
-            transitionAppear={true}
-            transitionAppearTimeout={500}
-            transitionEnter={false}
-            transitionLeave={false}>
-            <div className='error-message'>
-              <h1>SYNTAX ERROR:</h1>
-              <p>{error}</p>
-            </div>
-          </CSSTransitionGroup>
-          <div className='error-background' style={{height: `${height}px`}}></div>
-        </div>
+      <div>
+        <CSSTransitionGroup
+          transitionName='fast-transition'
+          transitionAppear={true}
+          transitionAppearTimeout={500}
+          transitionEnter={false}
+          transitionLeave={false}>
+          <div className='output__error-message'>
+            <h1>SYNTAX ERROR:</h1>
+            <p>{error}</p>
+          </div>
+        </CSSTransitionGroup>
+        <div className='output__error-background' style={{height: `${height}px`}}></div>
+      </div>
     )
   }
 }
