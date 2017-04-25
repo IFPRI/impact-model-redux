e851cff0717787fe39716863e697c7ec560ec922
Drew Bollinger <drew@developmentseed.org>
2017-04-25 15:26:22 -0400

    Add mailchimp form, close #192

--- app/assets/scripts/components/page-footer.js
+++ app/assets/scripts/components/page-footer.js
@@ -7,12 +7,37 @@ export class PageFooter extends React.Component {
       <footer className='page__footer'>
         <div className='row'>
           <div className='footer__contact'>
-            <h6 className='header--xsmall with-description'>Sign Up For Updates</h6>
-            <span className='sub-description'>Get updates whenever new information is added to the site.</span>
-            <form className='contact__input' action='temp'>
-              <input className='input--small' type='text' name='email' placeholder='Enter Your Email Address' />
-              <button className='button button--main button--small' type='submit'>Sign Up</button>
+            <div id="mc_embed_signup">
+            <form action="//ifpri.us15.list-manage.com/subscribe/post?u=475151f0c86ae50ba567b698a&amp;id=27d6c404c8" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
+              <div id="mc_embed_signup_scroll">
+                          <h2>Subscribe to our mailing list</h2>
+            <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
+            <div className="mc-field-group">
+                          <label htmlFor="mce-EMAIL">Email Address<span className="asterisk">*</span>
+            </label>
+                          <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
+            </div>
+            <div className="mc-field-group">
+                          <label htmlFor="mce-FNAME">First Name </label>
+                          <input type="text" name="FNAME" className="" id="mce-FNAME" />
+            </div>
+            <div className="mc-field-group">
+                          <label htmlFor="mce-LNAME">Last Name </label>
+                          <input type="text" name="LNAME" className="" id="mce-LNAME" />
+            </div>
+            <div className="mc-field-group">
+                          <label htmlFor="mce-MMERGE3">Organization </label>
+                          <input type="text" name="MMERGE3" className="" id="mce-MMERGE3" />
+            </div>
+                          <div id="mce-responses" className="clear">
+                                          <div className="response" id="mce-error-response" style={{ display: 'none' }}></div>
+                                          <div className="response" id="mce-success-response" style={{ display: 'none' }}></div>
+                          </div>
+              <div style={{position: 'absolute', left: '-5000px'}} aria-hidden="true"><input type="text" name="b_475151f0c86ae50ba567b698a_27d6c404c8" tabIndex="-1" value="" /></div>
+              <div className="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" /></div>
+              </div>
             </form>
+            </div>
           </div>
           <a className='footer__logo' target='_blank' href='http://www.ifpri.org/'>
             <img src='assets/graphics/content/ifpri.png' alt='IFPRI Logo' />
