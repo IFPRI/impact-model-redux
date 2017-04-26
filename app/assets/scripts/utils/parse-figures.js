cd7b86592b1d8e5a01438914ee3e11190b2cf331
Nick Bumbarger <nbumbarger@gmail.com>
2017-04-26 18:13:28 -0400

    Slight error improvements

--- app/assets/scripts/utils/parse-figures.js
+++ app/assets/scripts/utils/parse-figures.js
@@ -10,37 +10,39 @@ export const setupRenderer = (dispatch, previewer) => {
   renderer.table = (header, body) => {
     return `<div class="table-wrapper"><table><thead>${header}</thead><tbody>${body}</tbody></table></div>`
   }
-  renderer.code = (code, lang, escaped) => {
-    const data = yaml.load(code)
-    const id = `fig-${Math.random().toString(36).substring(7)}`
-    data.id = id
-    // convert dropdown values from string to array
-    Object.keys(data).forEach(dataKey => {
-      if (dataKey.match(/scenarios/)) {
-        data[dataKey] = data[dataKey].split(',').map(a => a.trim().toUpperCase())
-      }
-      if (dataKey.match(/dropdown/)) {
-        data[dataKey].values = data[dataKey].values.split(',').map(a => a.trim())
-      }
-    })
+  renderer.code = (code, lang) => {
+    try {
+      const data = yaml.load(code)
+      const id = `fig-${Math.random().toString(36).substring(7)}`
+      data.id = id
+      // convert dropdown values from string to array
+      Object.keys(data).forEach(dataKey => {
+        if (dataKey.match(/scenarios/)) {
+          data[dataKey] = data[dataKey].split(',').map(a => a.trim().toUpperCase())
+        }
+        if (dataKey.match(/dropdown/)) {
+          data[dataKey].values = data[dataKey].values.split(',').map(a => a.trim())
+        }
+      })
 
-    if (lang === 'chart') {
-      if (previewer) {
-        if (lang === 'chart') {
+      if (lang === 'chart') {
+        if (previewer) {
           dispatch(updatePreviewerChart(data, id))
           return `<div style="width:calc(${data.width} - 2rem)" class="${id} figure-container chart-figure"></div>`
         }
-        return ''
-      }
 
-      if (!previewer) {
-        dispatch(updateChart(data, id))
-        return `<div style="width:${data.width}" class="${id} figure-container chart-figure"></div>`
-      } else if (lang === 'map') {
-        dispatch(updateMap(data, id))
-        return `<div class="${id} figure-container map-figure"></div>`
+        if (!previewer) {
+          dispatch(updateChart(data, id))
+          return `<div style="width:${data.width}" class="${id} figure-container chart-figure"></div>`
+        } else if (lang === 'map') {
+          dispatch(updateMap(data, id))
+          return `<div class="${id} figure-container map-figure"></div>`
+        }
       }
+    } catch (err) {
+      throw new Error(err)
     }
   }
+
   return renderer
 }
