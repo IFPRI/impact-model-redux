8403014fb04573ff73ed9778e303000b0531e102
Nick Bumbarger <nbumbarger@gmail.com>
2017-05-05 10:50:14 -0400

    Replace object.values dependency with lodash values method

--- app/assets/scripts/main.js
+++ app/assets/scripts/main.js
@@ -11,10 +11,6 @@ import { Router, Route, IndexRoute, hashHistory, applyRouterMiddleware } from 'r
 import config from './config'
 import reducer from './reducers'
 
-// Additional Internet Explorer/ Safari shims
-import valuesShim from 'object.values'
-if (!Object.values) valuesShim.shim()
-
 const logger = createLogger({
   level: 'info',
   collapsed: true,
